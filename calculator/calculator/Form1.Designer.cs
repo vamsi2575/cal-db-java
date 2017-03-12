296a5c40b0be73a479205ad2e14b949b6ef3d03c
vamsi2575 <vamsikrishna2475@gmail.com>
2017-03-12 11:32:52 +0530

    basic calculator
    calculator using database in java

---
+++ calculator/calculator/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmd1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cmd2 = new System.Windows.Forms.Button();
+            this.cmd3 = new System.Windows.Forms.Button();
+            this.cmd4 = new System.Windows.Forms.Button();
+            this.cmd5 = new System.Windows.Forms.Button();
+            this.cmd6 = new System.Windows.Forms.Button();
+            this.cmd7 = new System.Windows.Forms.Button();
+            this.cmd8 = new System.Windows.Forms.Button();
+            this.cmd9 = new System.Windows.Forms.Button();
+            this.cmd0 = new System.Windows.Forms.Button();
+            this.cmde = new System.Windows.Forms.Button();
+            this.cmdd = new System.Windows.Forms.Button();
+            this.cmdp = new System.Windows.Forms.Button();
+            this.cmdm = new System.Windows.Forms.Button();
+            this.cmds = new System.Windows.Forms.Button();
+            this.cmddiv = new System.Windows.Forms.Button();
+            this.cmdclr = new System.Windows.Forms.Button();
+            this.cmdpow = new System.Windows.Forms.Button();
+            this.cmdoff = new System.Windows.Forms.Button();
+            this.cmdon = new System.Windows.Forms.Button();
+            this.cmdrem = new System.Windows.Forms.Button();
+            this.cmdsroot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmd1
+            // 
+            this.cmd1.Location = new System.Drawing.Point(12, 83);
+            this.cmd1.Name = "cmd1";
+            this.cmd1.Size = new System.Drawing.Size(40, 30);
+            this.cmd1.TabIndex = 0;
+            this.cmd1.Text = "1";
+            this.cmd1.UseVisualStyleBackColor = true;
+            this.cmd1.Click += new System.EventHandler(this.cmd1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 40);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cmd2
+            // 
+            this.cmd2.Location = new System.Drawing.Point(67, 83);
+            this.cmd2.Name = "cmd2";
+            this.cmd2.Size = new System.Drawing.Size(44, 30);
+            this.cmd2.TabIndex = 2;
+            this.cmd2.Text = "2";
+            this.cmd2.UseVisualStyleBackColor = true;
+            this.cmd2.Click += new System.EventHandler(this.cmd2_Click);
+            // 
+            // cmd3
+            // 
+            this.cmd3.Location = new System.Drawing.Point(126, 83);
+            this.cmd3.Name = "cmd3";
+            this.cmd3.Size = new System.Drawing.Size(45, 30);
+            this.cmd3.TabIndex = 3;
+            this.cmd3.Text = "3";
+            this.cmd3.UseVisualStyleBackColor = true;
+            this.cmd3.Click += new System.EventHandler(this.cmd3_Click);
+            // 
+            // cmd4
+            // 
+            this.cmd4.Location = new System.Drawing.Point(12, 134);
+            this.cmd4.Name = "cmd4";
+            this.cmd4.Size = new System.Drawing.Size(40, 33);
+            this.cmd4.TabIndex = 4;
+            this.cmd4.Text = "4";
+            this.cmd4.UseVisualStyleBackColor = true;
+            this.cmd4.Click += new System.EventHandler(this.cmd4_Click);
+            // 
+            // cmd5
+            // 
+            this.cmd5.Location = new System.Drawing.Point(67, 134);
+            this.cmd5.Name = "cmd5";
+            this.cmd5.Size = new System.Drawing.Size(44, 33);
+            this.cmd5.TabIndex = 5;
+            this.cmd5.Text = "5";
+            this.cmd5.UseVisualStyleBackColor = true;
+            this.cmd5.Click += new System.EventHandler(this.cmd5_Click);
+            // 
+            // cmd6
+            // 
+            this.cmd6.Location = new System.Drawing.Point(126, 134);
+            this.cmd6.Name = "cmd6";
+            this.cmd6.Size = new System.Drawing.Size(45, 33);
+            this.cmd6.TabIndex = 6;
+            this.cmd6.Text = "6";
+            this.cmd6.UseVisualStyleBackColor = true;
+            this.cmd6.Click += new System.EventHandler(this.cmd6_Click);
+            // 
+            // cmd7
+            // 
+            this.cmd7.Location = new System.Drawing.Point(12, 182);
+            this.cmd7.Name = "cmd7";
+            this.cmd7.Size = new System.Drawing.Size(40, 28);
+            this.cmd7.TabIndex = 7;
+            this.cmd7.Text = "7";
+            this.cmd7.UseVisualStyleBackColor = true;
+            this.cmd7.Click += new System.EventHandler(this.cmd7_Click);
+            // 
+            // cmd8
+            // 
+            this.cmd8.Location = new System.Drawing.Point(67, 182);
+            this.cmd8.Name = "cmd8";
+            this.cmd8.Size = new System.Drawing.Size(44, 30);
+            this.cmd8.TabIndex = 8;
+            this.cmd8.Text = "8";
+            this.cmd8.UseVisualStyleBackColor = true;
+            this.cmd8.Click += new System.EventHandler(this.cmd8_Click);
+            // 
+            // cmd9
+            // 
+            this.cmd9.Location = new System.Drawing.Point(126, 182);
+            this.cmd9.Name = "cmd9";
+            this.cmd9.Size = new System.Drawing.Size(45, 30);
+            this.cmd9.TabIndex = 9;
+            this.cmd9.Text = "9";
+            this.cmd9.UseVisualStyleBackColor = true;
+            this.cmd9.Click += new System.EventHandler(this.cmd9_Click);
+            // 
+            // cmd0
+            // 
+            this.cmd0.Location = new System.Drawing.Point(12, 228);
+            this.cmd0.Name = "cmd0";
+            this.cmd0.Size = new System.Drawing.Size(39, 33);
+            this.cmd0.TabIndex = 10;
+            this.cmd0.Text = "0";
+            this.cmd0.UseVisualStyleBackColor = true;
+            this.cmd0.Click += new System.EventHandler(this.cmd0_Click);
+            // 
+            // cmde
+            // 
+            this.cmde.Location = new System.Drawing.Point(67, 227);
+            this.cmde.Name = "cmde";
+            this.cmde.Size = new System.Drawing.Size(44, 34);
+            this.cmde.TabIndex = 11;
+            this.cmde.Text = "=";
+            this.cmde.UseVisualStyleBackColor = true;
+            this.cmde.Click += new System.EventHandler(this.cmde_Click);
+            // 
+            // cmdd
+            // 
+            this.cmdd.Location = new System.Drawing.Point(126, 227);
+            this.cmdd.Name = "cmdd";
+            this.cmdd.Size = new System.Drawing.Size(45, 34);
+            this.cmdd.TabIndex = 12;
+            this.cmdd.Text = ".";
+            this.cmdd.UseVisualStyleBackColor = true;
+            this.cmdd.Click += new System.EventHandler(this.cmdd_Click);
+            // 
+            // cmdp
+            // 
+            this.cmdp.Location = new System.Drawing.Point(197, 83);
+            this.cmdp.Name = "cmdp";
+            this.cmdp.Size = new System.Drawing.Size(42, 30);
+            this.cmdp.TabIndex = 13;
+            this.cmdp.Text = "+";
+            this.cmdp.UseVisualStyleBackColor = true;
+            this.cmdp.Click += new System.EventHandler(this.cmdp_Click);
+            // 
+            // cmdm
+            // 
+            this.cmdm.Location = new System.Drawing.Point(197, 134);
+            this.cmdm.Name = "cmdm";
+            this.cmdm.Size = new System.Drawing.Size(42, 33);
+            this.cmdm.TabIndex = 14;
+            this.cmdm.Text = "*";
+            this.cmdm.UseVisualStyleBackColor = true;
+            this.cmdm.Click += new System.EventHandler(this.cmdm_Click);
+            // 
+            // cmds
+            // 
+            this.cmds.Location = new System.Drawing.Point(197, 182);
+            this.cmds.Name = "cmds";
+            this.cmds.Size = new System.Drawing.Size(42, 28);
+            this.cmds.TabIndex = 15;
+            this.cmds.Text = "-";
+            this.cmds.UseVisualStyleBackColor = true;
+            this.cmds.Click += new System.EventHandler(this.cmds_Click);
+            // 
+            // cmddiv
+            // 
+            this.cmddiv.Location = new System.Drawing.Point(197, 228);
+            this.cmddiv.Name = "cmddiv";
+            this.cmddiv.Size = new System.Drawing.Size(42, 33);
+            this.cmddiv.TabIndex = 16;
+            this.cmddiv.Text = "/";
+            this.cmddiv.UseVisualStyleBackColor = true;
+            this.cmddiv.Click += new System.EventHandler(this.cmddiv_Click);
+            // 
+            // cmdclr
+            // 
+            this.cmdclr.Location = new System.Drawing.Point(8, 31);
+            this.cmdclr.Name = "cmdclr";
+            this.cmdclr.Size = new System.Drawing.Size(43, 30);
+            this.cmdclr.TabIndex = 17;
+            this.cmdclr.Text = "c";
+            this.cmdclr.UseVisualStyleBackColor = true;
+            this.cmdclr.Click += new System.EventHandler(this.cmdclr_Click);
+            // 
+            // cmdpow
+            // 
+            this.cmdpow.Location = new System.Drawing.Point(12, 275);
+            this.cmdpow.Name = "cmdpow";
+            this.cmdpow.Size = new System.Drawing.Size(40, 36);
+            this.cmdpow.TabIndex = 18;
+            this.cmdpow.Text = "^";
+            this.cmdpow.UseVisualStyleBackColor = true;
+            this.cmdpow.Click += new System.EventHandler(this.cmdpow_Click);
+            // 
+            // cmdoff
+            // 
+            this.cmdoff.Location = new System.Drawing.Point(263, 83);
+            this.cmdoff.Name = "cmdoff";
+            this.cmdoff.Size = new System.Drawing.Size(41, 30);
+            this.cmdoff.TabIndex = 19;
+            this.cmdoff.Text = "OFF";
+            this.cmdoff.UseVisualStyleBackColor = true;
+            this.cmdoff.Click += new System.EventHandler(this.cmdoff_Click);
+            // 
+            // cmdon
+            // 
+            this.cmdon.Location = new System.Drawing.Point(263, 134);
+            this.cmdon.Name = "cmdon";
+            this.cmdon.Size = new System.Drawing.Size(41, 33);
+            this.cmdon.TabIndex = 20;
+            this.cmdon.Text = "AC";
+            this.cmdon.UseVisualStyleBackColor = true;
+            this.cmdon.Click += new System.EventHandler(this.cmdon_Click);
+            // 
+            // cmdrem
+            // 
+            this.cmdrem.Location = new System.Drawing.Point(67, 275);
+            this.cmdrem.Name = "cmdrem";
+            this.cmdrem.Size = new System.Drawing.Size(43, 36);
+            this.cmdrem.TabIndex = 21;
+            this.cmdrem.Text = "%";
+            this.cmdrem.UseVisualStyleBackColor = true;
+            this.cmdrem.Click += new System.EventHandler(this.cmdrem_Click);
+            // 
+            // cmdsroot
+            // 
+            this.cmdsroot.Location = new System.Drawing.Point(126, 275);
+            this.cmdsroot.Name = "cmdsroot";
+            this.cmdsroot.Size = new System.Drawing.Size(45, 35);
+            this.cmdsroot.TabIndex = 22;
+            this.cmdsroot.Text = "sqrt";
+            this.cmdsroot.UseVisualStyleBackColor = true;
+            this.cmdsroot.Click += new System.EventHandler(this.cmdsroot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 358);
+            this.Controls.Add(this.cmdsroot);
+            this.Controls.Add(this.cmdrem);
+            this.Controls.Add(this.cmdon);
+            this.Controls.Add(this.cmdoff);
+            this.Controls.Add(this.cmdpow);
+            this.Controls.Add(this.cmdclr);
+            this.Controls.Add(this.cmddiv);
+            this.Controls.Add(this.cmds);
+            this.Controls.Add(this.cmdm);
+            this.Controls.Add(this.cmdp);
+            this.Controls.Add(this.cmdd);
+            this.Controls.Add(this.cmde);
+            this.Controls.Add(this.cmd0);
+            this.Controls.Add(this.cmd9);
+            this.Controls.Add(this.cmd8);
+            this.Controls.Add(this.cmd7);
+            this.Controls.Add(this.cmd6);
+            this.Controls.Add(this.cmd5);
+            this.Controls.Add(this.cmd4);
+            this.Controls.Add(this.cmd3);
+            this.Controls.Add(this.cmd2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cmd1);
+            this.Name = "Form1";
+            this.Text = "calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmd1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button cmd2;
+        private System.Windows.Forms.Button cmd3;
+        private System.Windows.Forms.Button cmd4;
+        private System.Windows.Forms.Button cmd5;
+        private System.Windows.Forms.Button cmd6;
+        private System.Windows.Forms.Button cmd7;
+        private System.Windows.Forms.Button cmd8;
+        private System.Windows.Forms.Button cmd9;
+        private System.Windows.Forms.Button cmd0;
+        private System.Windows.Forms.Button cmde;
+        private System.Windows.Forms.Button cmdd;
+        private System.Windows.Forms.Button cmdp;
+        private System.Windows.Forms.Button cmdm;
+        private System.Windows.Forms.Button cmds;
+        private System.Windows.Forms.Button cmddiv;
+        private System.Windows.Forms.Button cmdclr;
+        private System.Windows.Forms.Button cmdpow;
+        private System.Windows.Forms.Button cmdoff;
+        private System.Windows.Forms.Button cmdon;
+        private System.Windows.Forms.Button cmdrem;
+        private System.Windows.Forms.Button cmdsroot;
+    }
+}
+
---
+++ calculator/calculator/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        string input = string.Empty;
+        string op1 = string.Empty;
+        string op2 = string.Empty;
+        char Operator;
+        double res=0.0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmd1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += "1";
+            this.textBox1.Text += input;
+        }
+
+        private void cmd2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += "2";
+            this.textBox1.Text += input;
+
+        }
+
+        private void cmd3_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += "3";
+            this.textBox1.Text += input;
+
+        }
+
+        private void cmd4_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += "4";
+            this.textBox1.Text += input;
+        }
+
+        private void cmd5_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += "5";
+            this.textBox1.Text += input;
+        }
+
+        private void cmd6_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += "6";
+            this.textBox1.Text += input;
+        }
+
+        private void cmd7_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += "7";
+            this.textBox1.Text += input;
+        }
+
+        private void cmd8_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += "8";
+            this.textBox1.Text += input;
+        }
+
+        private void cmd9_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += "9";
+            this.textBox1.Text += input;
+        }
+
+        private void cmd0_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += "0";
+            this.textBox1.Text += input;
+        }
+
+        private void cmde_Click(object sender, EventArgs e)
+        {
+            op2 = input;
+            double num1, num2;
+            double.TryParse(op1, out num1);
+            double.TryParse(op2, out num2);
+            if (Operator == '+')
+            {
+                res = num1 + num2;
+                textBox1.Text = res.ToString();
+            }
+            else if(Operator=='-')
+            {
+                res = num1 - num2;
+                textBox1.Text = res.ToString();
+            }
+            else if (Operator == '*')
+            {
+                res = num1 * num2;
+                textBox1.Text = res.ToString();
+            }
+            else if (Operator == '/')
+            {
+                if (num2 == 0)
+                    textBox1.Text = "not possible";
+                else
+                {
+                    res = num1 / num2;
+                    textBox1.Text = res.ToString();
+                }
+            }
+            else if (Operator == '^')
+            {
+
+                res = System.Math.Pow(num1, num2);
+                textBox1.Text = res.ToString();
+            }
+            else if (Operator == '%')
+            {
+                res = num1 % num2;
+                textBox1.Text = res.ToString();
+            }
+            input = "";
+
+        }
+
+        private void cmdd_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            input += ".";
+            this.textBox1.Text += input;
+        }
+
+        private void cmdp_Click(object sender, EventArgs e)
+        {
+            op1 = input;
+            Operator = '+';
+            input = string.Empty;
+        }
+
+        private void cmdm_Click(object sender, EventArgs e)
+        {
+            op1 = input;
+            Operator = '*';
+            input = string.Empty;
+        }
+
+        private void cmds_Click(object sender, EventArgs e)
+        {
+            op1 = input;
+            Operator = '-';
+            input = string.Empty;
+        }
+
+        private void cmddiv_Click(object sender, EventArgs e)
+        {
+            op1 = input;
+            Operator = '/';
+            input = string.Empty;
+        }
+        private void cmdpow_Click(object sender, EventArgs e)
+        {
+            op1 = input;
+            Operator = '^';
+            input = string.Empty;
+        }
+        private void cmdrem_Click(object sender, EventArgs e)
+        {
+            op1 = input;
+            Operator = '%';
+            input = string.Empty;
+        }
+        
+
+        private void cmdsroot_Click(object sender, EventArgs e)
+        {
+            string op3 = string.Empty;
+            op3 = input;
+            double num3;
+            double.TryParse(op3,out num3);
+            res = System.Math.Pow(num3, 0.5);
+            textBox1.Text = res.ToString();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdclr_Click(object sender, EventArgs e)
+        {
+            op1 = string.Empty;
+            op2 = string.Empty;
+            input = string.Empty;
+            textBox1.Text = string.Empty;
+        }
+
+
+        private void cmdoff_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox1.Text = "";
+            cmd0.Enabled = false;
+            cmd1.Enabled = false;
+            cmd2.Enabled = false;
+            cmd3.Enabled = false;
+            cmd4.Enabled = false;
+            cmd5.Enabled = false;
+            cmd6.Enabled = false;
+            cmd7.Enabled = false;
+            cmd8.Enabled = false;
+            cmd9.Enabled = false;
+            cmde.Enabled = false;
+        }
+
+        private void cmdon_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            textBox1.Text = "";
+            cmd0.Enabled = true;
+            cmd1.Enabled = true;
+            cmd2.Enabled = true;
+            cmd3.Enabled = true;
+            cmd4.Enabled = true;
+            cmd5.Enabled = true;
+            cmd6.Enabled = true;
+            cmd7.Enabled = true;
+            cmd8.Enabled = true;
+            cmd9.Enabled = true;
+            cmde.Enabled = true;
+        }
+
+
+    }
+}
